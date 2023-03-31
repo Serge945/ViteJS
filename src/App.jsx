5fad7f9d94784dad27cc0054a2d592de6809147a
serge patrick acka <sergeacka@gmail.com>
2023-03-31 08:46:56 +0200

    props-type

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,52 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyTitle from './Mytitle' 
+import PokemonCard from './components/PokemonCard'
+
+// mytitle represente le composant et ./Mytitle represente le chemin et ca qui est precis parce que le chemin est précis 
+// import MyTitle from './components/PokemonCard'
+//   quand tu appelles deux fois deux composants avec le meme nom ca ne marche pas 
+//  le nom des composants doit être pareil 
+// app jsx est un composant 
+
+
+const pokemon = "bulbasaur";  
+ const pokemonList = [
+     {
+     name: "bulbasaur",
+     imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+        {
+          name: "pikachu",
+        },
+      ];
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
+    return (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <MyTitle/>
+        <PokemonCard pokemon = {pokemonList[0]}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+  
+)
+    }
 
 export default App
+
+// PokemonCard est un composant React et pokemon est une prop passée à ce composant.
+
+// Dans cette ligne de code : <PokemonCard pokemon={pokemonList[0]}/>, la prop pokemon est égale
+//  à l'objet du premier élément de pokemonList. Cet objet a deux propriétés : name qui est égal
+//   à "bulbasaur", et imgSrc qui est l'URL de l'image pour "bulbasaur".
+
+// Le composant PokemonCard peut utiliser la prop pokemon pour afficher les informations
+//  du pokémon de manière appropriée dans l'interface utilisateur.
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,93 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+// import PokemonCard from '../components/PokemonCard';
+import MyTitle from '../Mytitle';
+ 
+
+//  const pokemon = "bulbasaur";  
+//  const pokemonList = [
+//      {
+//      name: "bulbasaur",
+//      imgSrc:
+//             "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//         },
+//         {
+//           name: "mew",
+//         },
+//         {
+//           name: "pikachu",
+//         },
+//       ];
+
+        
+      
+
+// chaque component a son jsx qui ensuite est redirigé vers une fonction App
+//  l'intéret du jsx est de pouvoir faire du js dans du html
+//  le but du return est de renvoyer le jsx  
+
+// function PokemonCardStatic(){
+//     const pokemon=pokemonList[0]; 
+//     const image = pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>;
+
+//     return (
+//         <div>
+//     <h2>{pokemon.name}</h2>
+//       {image}
+    
+//     </div>
+//     )
+// }
+
+function PokemonCard(props){
+    console.log(props)
+    const pokemon = props.pokemon;
+    const image = pokemon.imgSrc? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      );
+
+return (
+<div>
+<h2>{pokemon.name}</h2>
+{image}
+</div>
+);
+}
+
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
+};
+
+// explication de la ternaire 
+// Cette ligne de code utilise une instruction conditionnelle (ou ternaire) pour décider d'afficher l'image du Pokémon ou un paragraphe avec des points d'interrogation si la propriété imgSrc n'est pas définie pour le Pokémon en question.
+
+// La condition vérifie si la propriété imgSrc existe pour le Pokémon actuel en utilisant 
+// l'opérateur ternaire ?. Si elle existe, elle renvoie un élément img avec l'URL 
+// de l'image stockée dans la propriété imgSrc et le nom du Pokémon stocké dans la propriété name avec 
+// l'attribut alt. Si elle n'existe pas, elle renvoie un élément p avec le texte "???" à l'intérieur.
+
+
+// function PokemonCard() {
+//     const pokemon = pokemonList[1]; // Récupère le deuxième Pokémon de la liste
+  
+//     // Si la propriété `imgSrc` existe pour le Pokémon actuel, affiche l'image. Sinon, affiche un paragraphe avec des points d'interrogation.
+//     const image = pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>;
+  
+//     return (
+//       <div>
+//         <h2>{pokemon.name}</h2>
+//         {image}
+//       </div>
+//     );
+//   }
+  
+
+
+ 
+    export default PokemonCard
